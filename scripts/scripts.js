b43a52b3a90cfc45e33a0c7444a9b8407840505e
pbiggin <pbiggin@student.unimelb.edu.au>
2025-05-13 16:20:37 +1000

    added navigation hamburger js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,60 +1,159 @@
-
-// used copilot here to assist with creating this feature // 
+// used copilot here to assist with creating this feature //
 const products = [
-  { name: "Biscoff Gelato Cake", type: "gelato", image: "../images/placeholder.webp"  },
-  { name: "Black forest", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Bombe Alaska Gelato Cake", type: "gelato", image: "../images/placeholder.webp" },
-  { name: "Brunetti Classico Rocher", type: "gelato", image: "../images/placeholder.webp" },
-  { name: "Cassata Sicliana", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Chocolate Mud Cake", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Chocolate Souffle", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Classic Black Forest", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Classic White Forest", type: "standard", image: "../images/placeholder.webp" },
+  {
+    name: "Biscoff Gelato Cake",
+    type: "gelato",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Black forest",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Bombe Alaska Gelato Cake",
+    type: "gelato",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Brunetti Classico Rocher",
+    type: "gelato",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Cassata Sicliana",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Chocolate Mud Cake",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Chocolate Souffle",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Classic Black Forest",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Classic White Forest",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
   { name: "Con Amore", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Cookies & Cream Cheesecake", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Croquembuche", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Cookies & Cream Cheesecake",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Croquembuche",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Crostata", type: "standard", image: "../images/placeholder.webp" },
   { name: "Fleur", type: "premium", image: "../images/placeholder.webp" },
   { name: "Francese", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Giant Cannoli Cake", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Julio Cesere", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Lemon Mergingue Tart", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Giant Cannoli Cake",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Julio Cesere",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Lemon Mergingue Tart",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Lemon Tart", type: "standard", image: "../images/placeholder.webp" },
   { name: "Millefoglie", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Mixed Berry Charlotte", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Mixed Fruit Tart", type: "premium", image: "../images/placeholder.webp" },
-  { name: "New York cheesecake", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Nocciolato Cake", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Mixed Berry Charlotte",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Mixed Fruit Tart",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "New York cheesecake",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Nocciolato Cake",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Opera", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Pear & Cheese Crumble", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Pear & Cheese Crumble",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Profiterole", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Promesse D'amore Tower", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Rocher Gelato Cake", type: "gelato", image: "../images/placeholder.webp" },
+  {
+    name: "Promesse D'amore Tower",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Rocher Gelato Cake",
+    type: "gelato",
+    image: "../images/placeholder.webp",
+  },
   { name: "Royale", type: "standard", image: "../images/placeholder.webp" },
   { name: "Sotto Bosco", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Strawberry Cheesecake Gelato Cake", type: "gelato", image: "../images/placeholder.webp" },
-  { name: "Strawberry Tart", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Strawberry Cheesecake Gelato Cake",
+    type: "gelato",
+    image: "../images/placeholder.webp",
+  },
+  {
+    name: "Strawberry Tart",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Svizzera", type: "standard", image: "../images/placeholder.webp" },
-  { name: "Tiramisu Semifreddo", type: "premium", image: "../images/placeholder.webp" },
+  {
+    name: "Tiramisu Semifreddo",
+    type: "premium",
+    image: "../images/placeholder.webp",
+  },
   { name: "Torta", type: "premium", image: "../images/placeholder.webp" },
-  { name: "Torta Di Arancia", type: "standard", image: "../images/placeholder.webp" },
+  {
+    name: "Torta Di Arancia",
+    type: "standard",
+    image: "../images/placeholder.webp",
+  },
 ];
 
 let productsPerPage = 12;
 let currentPage = 0;
-let isExpanded = false
+let isExpanded = false;
 
 // Function to load products
 function loadProducts() {
-    const productList = document.getElementById("product-list");
-    const start = currentPage * productsPerPage;
-    const end = start + productsPerPage;
-    const productsToLoad = products.slice(start, end);
-  
-    productsToLoad.forEach((product) => {
-      // Determine the price based on the product type
+  const productList = document.getElementById("product-list");
+  const start = currentPage * productsPerPage;
+  const end = start + productsPerPage;
+  const productsToLoad = products.slice(start, end);
 
-      let price;
+  productsToLoad.forEach((product) => {
+    // Determine the price based on the product type
+
+    let price;
     if (product.type === "premium") {
       price = "From $46.00";
     } else if (product.type === "standard") {
@@ -64,74 +163,53 @@ function loadProducts() {
     } else {
       price = "Price not available";
     }
-      // Create product card
-      const productDiv = document.createElement("div");
-      productDiv.classList.add("product");
-  
-      productDiv.innerHTML = `
+    // Create product card
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
         <img src="${product.image}" alt="${product.name}" />
         <h3>${product.name}</h3>
         <p>${price}</p>
       `;
-  
-      productList.appendChild(productDiv);
-    });
-  
-    currentPage++;
-  
-    if (currentPage * productsPerPage >= products.length) {
-      document.getElementById("load-more").innerText = "Load Less";
-      isExpanded = true;
-    }
+
+    productList.appendChild(productDiv);
+  });
+
+  currentPage++;
+
+  if (currentPage * productsPerPage >= products.length) {
+    document.getElementById("load-more").innerText = "Load Less";
+    isExpanded = true;
   }
+}
 
 function loadLessProducts() {
-    const productList = document.getElementById("product-list");
-    const allProducts = Array.from(productList.children);
-  
-    // Remove products beyond the first batch
-    const productsToRemove = allProducts.slice(productsPerPage);
-    productsToRemove.forEach((product) => product.remove());
-  
-    currentPage = 1; // Reset to the first page
-    document.getElementById("load-more").innerText = "Load More";
-    isExpanded = false;
-  }
-  
+  const productList = document.getElementById("product-list");
+  const allProducts = Array.from(productList.children);
+
+  // Remove products beyond the first batch
+  const productsToRemove = allProducts.slice(productsPerPage);
+  productsToRemove.forEach((product) => product.remove());
+
+  currentPage = 1; // Reset to the first page
+  document.getElementById("load-more").innerText = "Load More";
+  isExpanded = false;
+}
 
 // load button listeners
 document.getElementById("load-more").addEventListener("click", () => {
-    if (isExpanded) {
-      loadLessProducts(); 
-    } else {
-      loadProducts(); 
-    }
-  });
+  if (isExpanded) {
+    loadLessProducts();
+  } else {
+    loadProducts();
+  }
+});
 
 loadProducts();
 
-const slider = document.querySelector('#drag');
-let mouseDown = false;
-let startX, scrollLeft;
-let startDragging = function (e) {
-    mouseDown = true;
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-};
 
-let stopDragging = function (event) {
-    mouseDown = false;
-};
-
-slider.addEventListener('mousemove', (e) => {
-    e.preventDefault();  if(!mouseDown) {
-        return;
-    }
-    const x = e.pageX - slider.offsetLeft;
-    const scroll = x - startX;
-    slider.scrollLeft = scrollLeft - scroll;
-});
-
-slider.addEventListener('mousedown', startDragging, false);
-slider.addEventListener('mouseup', stopDragging, false);
-slider.addEventListener('mouseleave', stopDragging, false);
+function toggleOverlay () {
+  const overlay = document.querySelector(".overlay");
+    overlay.classList.toggle("hidden");
+}
