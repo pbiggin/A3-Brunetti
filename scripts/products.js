6d3e3f77a1c8d06b62f52f5d730619a17cfbdf8d
pbiggin <pbiggin@student.unimelb.edu.au>
2025-05-19 16:31:47 +1000

    modified hero svg

---
+++ scripts/products.js
@@ -0,0 +1,140 @@
+// used github copilot here to assist with creating this feature //
+const products = [
+    {
+      name: "Biscoff Gelato Cake",
+      type: "gelato",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Black forest",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Bombe Alaska Gelato Cake",
+      type: "gelato",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Brunetti Classico Rocher",
+      type: "gelato",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Cassata Sicliana",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Chocolate Mud Cake",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Chocolate Souffle",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Classic Black Forest",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Classic White Forest",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Con Amore", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Cookies & Cream Cheesecake",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Croquembuche",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Crostata", type: "standard", image: "../images/placeholder.webp" },
+    { name: "Fleur", type: "premium", image: "../images/placeholder.webp" },
+    { name: "Francese", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Giant Cannoli Cake",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Julio Cesere",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Lemon Mergingue Tart",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Lemon Tart", type: "standard", image: "../images/placeholder.webp" },
+    { name: "Millefoglie", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Mixed Berry Charlotte",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Mixed Fruit Tart",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "New York cheesecake",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Nocciolato Cake",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Opera", type: "standard", image: "../images/placeholder.webp" },
+    {
+      name: "Pear & Cheese Crumble",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Profiterole", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Promesse D'amore Tower",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Rocher Gelato Cake",
+      type: "gelato",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Royale", type: "standard", image: "../images/placeholder.webp" },
+    { name: "Sotto Bosco", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Strawberry Cheesecake Gelato Cake",
+      type: "gelato",
+      image: "../images/placeholder.webp",
+    },
+    {
+      name: "Strawberry Tart",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Svizzera", type: "standard", image: "../images/placeholder.webp" },
+    {
+      name: "Tiramisu Semifreddo",
+      type: "premium",
+      image: "../images/placeholder.webp",
+    },
+    { name: "Torta", type: "premium", image: "../images/placeholder.webp" },
+    {
+      name: "Torta Di Arancia",
+      type: "standard",
+      image: "../images/placeholder.webp",
+    },
+  ];
