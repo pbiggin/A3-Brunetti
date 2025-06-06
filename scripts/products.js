cd96f691ac0b931a72f1bf6a6659ef9b3ad9a1fa
pbiggin <pbiggin@student.unimelb.edu.au>
2025-06-06 16:53:53 +1000

    fixing missing image links

--- scripts/products.js
+++ scripts/products.js
@@ -397,13 +397,13 @@ const products = [
 ];
 
 const productImages = {
-  "Biscoff Gelato Cake": "/images/Biscoff-Gelato-Cake.jpg",
-  "Black Forest": "/images/Black-Forest.jpg",
-  "Bombe Alaska Gelato Cake": "/images/Bombe-alaska.jpg",
-  "Brunetti Classico Rocher": "/images/Brunetti-rocher.jpg",
-  "Cassata Siciliana": "/images/Casata.jpg",
-  "Choc Berry Slice": "/images/Choc-berry-slice.jpg",
-  "Chocolate Mousse Cake": "/images/Chocolate-Mousse-Cake.jpg",
+  "Biscoff Gelato Cake": "images/Biscoff-Gelato-Cake.jpg",
+  "Black Forest": "images/Black-Forest.jpg",
+  "Bombe Alaska Gelato Cake": "images/Bombe-alaska.jpg",
+  "Brunetti Classico Rocher": "images/Brunetti-rocher.jpg",
+  "Cassata Siciliana": "images/Casata.jpg",
+  "Choc Berry Slice": "images/Choc-berry-slice.jpg",
+  "Chocolate Mousse Cake": "images/Chocolate-Mousse-Cake.jpg",
 };
 
 const suggestions = [
